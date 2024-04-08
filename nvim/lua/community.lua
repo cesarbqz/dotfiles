1f344f1b32a7813aa2833561ca1b17c2f15444e7
César Bogado <qz@Cesars-MacBook-Pro.local>
2024-04-08 00:17:39 -0400

    Added terraform,ansible,python,sql and yaml community packs

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -6,5 +6,10 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.recipes.telescope-nvchad-theme" }
+  { import = "astrocommunity.recipes.telescope-nvchad-theme" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.yaml" },
 }
